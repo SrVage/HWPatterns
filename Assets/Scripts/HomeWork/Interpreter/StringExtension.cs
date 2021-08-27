96ac72df66195b8cc168ae7b75e26c12c4f1f528
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2021-08-27 11:39:56 +0700

    2

---
+++ Assets/Scripts/HomeWork/Interpreter/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace
+{
+    public static class StringExtension
+    {
+        public static string ShortText(this string str)
+        {
+            var number = float.Parse(str);
+            if (number >= 1000 && number<1000000)
+            {
+                number = number / 1000;
+                return number.ToString() + "K";
+            }
+
+            if (number >= 1000000)
+            {
+                number = number / 1000000;
+                return number.ToString() + "M";
+            }
+
+            return str;
+        }
+    }
+}
